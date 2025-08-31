5ca5872676f4b40e891ab71cb3fd70467e41f638
kkh4606 <winningelevendatabackup@gmail.com>
2025-08-31 22:34:08 +0630

    fix

--- app/config.py
+++ app/config.py
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
     database_password: str
     database_name: str
 
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    secret_key: str = "sadjfljalsjfdljasljfdlajsd"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 60
 
     class Config:
         env_file = ".env"
