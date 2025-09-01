19b4698fc6760e5cf17b3401d379e46f11120695
kkh4606 <winningelevendatabackup@gmail.com>
2025-09-01 22:44:22 +0630

    add middleware

--- app/main.py
+++ app/main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from app.routes import auth, user, todo
 from . import models
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(todo.router)
